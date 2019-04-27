812ccf8d01730f25d69064d2176eeec720c52664
zhaoyue <1833979800@qq.com>
2019-04-27 14:43:55 +0800

    commit

---
+++ admin/src/service/product.jsx
@@ -0,0 +1,16 @@
+/*
+* @Author: Rosen
+* @Date:   2017-04-05 11:07:34
+* @Last Modified by:   Rosen
+* @Last Modified time: 2017-04-05 17:47:33
+*/
+
+'use strict';
+
+import MMUtile from 'util/mm.jsx';
+
+const _mm = new MMUtile();
+
+export default class Product{
+
+}
